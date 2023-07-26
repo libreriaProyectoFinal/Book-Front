1166b106f6fe7bc4427304b5dff1a8f6c7b392d0
theinventor1 <37818640+theinventor1@users.noreply.github.com>
2023-07-25 23:23:46 -0400

    s

--- src/components/Paginate/Paginate.jsx
+++ src/components/Paginate/Paginate.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import './Paginate.css';
 
-const Pagination = ({ currentPage, totalBooks, librosPorPagina, onPageChange }) => {
-  const totalPages = Math.ceil(totalBooks / librosPorPagina);
-  const pageNumbers = [];
+const Pagination = ({ libros, onPageChange}) => {
+ const { paginaActual, librosPorPagina, totalLibros , totalPaginas} = libros;
+ const totalPages = Math.ceil(totalLibros / librosPorPagina);
+
+ const pageNumbers = [];
 
   // Limitar la paginación a un máximo de 7 páginas
-  const maxDisplayedPages = 7;
+  const maxDisplayedPages = 20;
   const middlePage = Math.floor(maxDisplayedPages / 2);
-  let startPage = currentPage - middlePage;
-  let endPage = currentPage + middlePage;
+  let startPage = paginaActual - middlePage;
+  let endPage = paginaActual + middlePage;
 
   if (startPage <= 0) {
     startPage = 1;
-    endPage = Math.min(totalPages, maxDisplayedPages);
+    endPage = Math.min(totalPaginas, maxDisplayedPages);
   }
 
-  if (endPage > totalPages) {
-    endPage = totalPages;
-    startPage = Math.max(1, totalPages - maxDisplayedPages + 1);
+  if (endPage > totalPaginas) {
+    endPage = totalPaginas;
+    startPage = Math.max(1, totalPaginas - maxDisplayedPages + 1);
   }
 
   for (let i = startPage; i <= endPage; i++) {
@@ -28,16 +30,16 @@ const Pagination = ({ currentPage, totalBooks, librosPorPagina, onPageChange }) 
   return (
     <ul className="pagination">
       {/* Agregar botón "Anterior" */}
-      {currentPage > 1 && (
+      {paginaActual > 1 && (
         <li className="page-item">
-          <button className="page-link" onClick={() => onPageChange(currentPage - 1)}>
+          <button className="page-link" onClick={() => onPageChange(paginaActual - 1)}>
             ANTERIOR
           </button>
         </li>
       )}
 
       {pageNumbers.map((number) => (
-        <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
+        <li key={number} className={`page-item ${paginaActual === number ? 'active' : ''}`}>
           <button className="page-link" onClick={() => onPageChange(number)}>
             {number}
           </button>
@@ -45,9 +47,9 @@ const Pagination = ({ currentPage, totalBooks, librosPorPagina, onPageChange }) 
       ))}
 
       {/* Agregar botón "Siguiente" */}
-      {currentPage < totalPages && (
+      {paginaActual < totalPages && (
         <li className="page-item">
-          <button className="page-link" onClick={() => onPageChange(currentPage + 1)}>
+          <button className="page-link" onClick={() => onPageChange(paginaActual + 1)}>
             SIGUIENTE
           </button>
         </li>
