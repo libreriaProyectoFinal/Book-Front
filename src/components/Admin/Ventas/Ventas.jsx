f33599d483c20f9bf0b17b0de4632953fe914757
Waldir merjildo <muwaldir@gmail.com>
2023-08-09 16:09:31 -0500

    estableci la conexion de ventas de libros con el back

--- src/components/Admin/Ventas/Ventas.jsx
+++ src/components/Admin/Ventas/Ventas.jsx
@@ -1,28 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { urlBack } from '../../../redux/actions/actions';
-import { format } from 'date-fns';
-import styles from './Ventas.module.css'; // Importa el archivo CSS module
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { urlBack } from "../../../redux/actions/actions";
+import { format } from "date-fns";
+import styles from "./Ventas.module.css"; // Importa el archivo CSS module
 
 const VentasComponent = () => {
-  const [ventas, setVentas] = useState([]);
+  const [compras, setCompras] = useState([]);
   const [montoTotal, setMontoTotal] = useState(0);
   const [cantidadTotal, setCantidadTotal] = useState(0);
 
   useEffect(() => {
     const getCompras = async () => {
-      const user = localStorage.getItem('user');
+      const user = localStorage.getItem("user");
       const dataUser = JSON.parse(user);
       try {
-        const response = await axios.get(urlBack + '/ventas', {
+        const response = await axios.get(urlBack + "/ventas", {
           headers: { Authorization: dataUser.accessToken },
         });
-
-        setVentas(response.data.ventas);
-        setMontoTotal(response.data.montoTotalVentas);
-        setCantidadTotal(response.data.cantidadTotalLibrosVendidos);
+        console.log(response.data);
+        setCompras(response.data.compras);
+        setCantidadTotal(
+          response.data.compras
+            .flatMap((compra) => compra.detalleocs)
+            .reduce((total, detalle) => total + detalle.cant, 0)
+        );
+        setMontoTotal(
+          response.data.compras.reduce(
+            (subtotal, venta) => subtotal + venta.valortotaloc,
+            0
+          )
+        );
       } catch (error) {
-        console.error('Error al obtener las ventas:', error);
+        console.error("Error al obtener las ventas:", error);
       }
     };
 
@@ -31,7 +40,7 @@ const VentasComponent = () => {
 
   const handleEliminarVenta = async (id) => {
     try {
-      const user = localStorage.getItem('user');
+      const user = localStorage.getItem("user");
       const dataUser = JSON.parse(user);
 
       await axios.delete(urlBack + `/ventas/${id}`, {
@@ -39,47 +48,68 @@ const VentasComponent = () => {
       });
 
       // Actualizar la lista de ventas después de eliminar
-      const updatedVentas = ventas.filter((venta) => venta.id !== id);
-      setVentas(updatedVentas);
+      const updatedVentas = compras.filter((venta) => venta.id !== id);
+      setCompras(updatedVentas);
     } catch (error) {
-      console.error('Error al eliminar la venta:', error);
+      console.error("Error al eliminar la venta:", error);
     }
   };
 
   return (
-    <div className={styles['ventas-container']}>
-      <h1 className={styles['ventas-title']}>Ventas</h1>
-      <div className={styles['total-container']}>
-        <p className={styles['monto-total']}>Monto Total de la Venta: ${montoTotal}</p>
-        <p className={styles['cantidad-total']}>Cantidad Total Vendida: {cantidadTotal}</p>
+    <div className={styles["ventas-container"]}>
+      <h1 className={styles["ventas-title"]}>Ventas</h1>
+      <div className={styles["total-container"]}>
+        <p className={styles["monto-total"]}>
+          Monto Total de la Venta: ${montoTotal}
+        </p>
+        <p className={styles["cantidad-total"]}>
+          Cantidad Total Vendida de Libros: {cantidadTotal}
+        </p>
       </div>
 
-      <div className={styles['table-container']}>
-        <table className={styles['ventas-table']}>
+      <div className={styles["table-container"]}>
+        <table className={styles["ventas-table"]}>
           <thead>
             <tr>
               <th>Fecha</th>
               <th>Estado</th>
               <th>Usuario</th>
               <th>Libros</th>
-              <th>Cantidad</th>
-              <th>Precio</th>
-              <th>Acciones</th>
+              <th>Cantidad Comprada</th>
+              <th>Pago total</th>
+              {/* <th>Acciones</th> */}
             </tr>
           </thead>
           <tbody>
-            {ventas.map((venta) => {
+            {compras.map((venta) => {
               return (
                 <tr key={venta.id}>
-                  <td>{format(new Date(venta.fechahoraoc), 'yyyy-MM-dd HH:mm:ss')}</td>
+                  <td>
+                    {format(new Date(venta.fechahoraoc), "yyyy-MM-dd HH:mm:ss")}
+                  </td>
                   <td>{venta.estadooc}</td>
                   <td>{venta.usuario?.name}</td>
-                  <td>{venta.nombresLibros.join(', ')}</td>
-                  <td>{venta.totalCant}</td>
-                  <td>{venta.valortotaloc}</td>
                   <td>
-                    <button className={styles['eliminar-button']} onClick={() => handleEliminarVenta(venta.id)}>Eliminar</button>
+                    {venta.detalleocs
+                      .map((detalle) => detalle.libro.nombrelibro)
+                      .join(", ") || ""}
                   </td>
+                  <td>
+                    {venta.detalleocs.reduce(
+                      (subtotal, detalle) => subtotal + detalle.cant,
+                      0
+                    )}
+                  </td>
+
+                  <td>{venta.valortotaloc}</td>
+                  {/* <td>
+                    <button
+                      className={styles["eliminar-button"]}
+                      onClick={() => handleEliminarVenta(venta.id)}
+                    >
+                      Eliminar
+                    </button>
+                  </td> */}
                 </tr>
               );
             })}
